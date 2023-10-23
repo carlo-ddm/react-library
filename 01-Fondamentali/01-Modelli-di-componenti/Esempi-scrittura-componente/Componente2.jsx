a4845204c959cba077807ab826071fd7e6acdb8c
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2023-10-23 21:46:02 +0200

    Fondamentali

---
+++ 01-Fondamentali/01-Modelli-di-componenti/Esempi-scrittura-componente/Componente2.jsx
@@ -0,0 +1,8 @@
+// Esempio 2: Funzione Dichiarativa con Export Diretto
+export default function Component() {
+    return (
+        <div></div>
+    )
+};
+
+/** Qui, esporto il mio componente direttamente durante la dichiarazione. Questo è più conciso, ma limita la mia capacità di aggiungere logica extra prima dell'istruzione export. */
---
+++ 01-Fondamentali/01-Modelli-di-componenti/Esempi-scrittura-componente/Componente4.jsx
@@ -0,0 +1,6 @@
+// Esempio 4: Funzione Freccia Concisione Massima
+const Component = () =>  <div></div>;
+
+export default Component;
+
+/** Questo è il modello più snello. Utilizzo una funzione freccia e ometto le parentesi graffe e l'istruzione return. Questo è possibile solo quando il corpo della funzione consiste in una singola espressione. È il metodo più "elegante" per definire un componente se non ho bisogno di alcuna logica aggiuntiva. */
