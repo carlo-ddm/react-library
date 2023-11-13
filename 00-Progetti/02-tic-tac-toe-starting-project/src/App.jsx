18b9a23a6e4776ab11e1c2a4d5bba53ab68c3b97
Carlo Duminuco Del Monte <carloduminuco90@gmail.com>
2023-11-13 14:47:49 +0100

    modifiche

--- 00-Progetti/02-tic-tac-toe-starting-project/src/App.jsx
+++ 00-Progetti/02-tic-tac-toe-starting-project/src/App.jsx
@@ -2,26 +2,57 @@ import { useState } from "react";
 import Player from "./Player";
 import GameBoard from "./GameBoard";
 import Log from "./Log";
+import { WINNING_COMBINATIONS } from "./winning-combination";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
 
-  const handleUpdateSymbol = () => {
-    setActivePlayer(curActivSymbol => {
-      return curActivSymbol === 'X' ? 'O' : 'X';
+  let activePlayer = deriveActivePlayer(gameTurns);
+
+  WINNING_COMBINATIONS.forEach(combination => {
+    const firstSquare
+    const secondSquare
+    const thirdSquare
+  });
+
+  function deriveActivePlayer(gameTurns) {
+    let currentPlayer = "X";
+    if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+      currentPlayer = "O";
+    }
+    return currentPlayer;
+  }
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      activePlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
     });
-  };
+  }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player defaultName="Player 1" isActive={activePlayer === 'X'} />
-          <Player defaultName="Player 2" isActive={activePlayer === 'O'}/>
+          <Player
+            defaultName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            defaultName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleUpdateSymbol} symbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- 00-Progetti/02-tic-tac-toe-starting-project/src/GameBoard.jsx
+++ 00-Progetti/02-tic-tac-toe-starting-project/src/GameBoard.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, symbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  const symbolHandler = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = symbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
-  };
+  // const symbolHandler = (rowIndex, colIndex) => {
+  //   if (gameBoard[rowIndex][colIndex] === null) {
+
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = symbol;
+  //       return updatedBoard;
+  //     });
+
+  //     onSelectSquare(); // modifico il simbolo con l'handler dichiarato in App e utilizzato qui
+  //   }
+  // };
+
+  let gameBoard = initialGameBoard;
+
+  turns.forEach((turn) => {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  });
 
   return (
     <ol id="game-board">
@@ -28,8 +39,9 @@ export default function GameBoard({ onSelectSquare, symbol }) {
               <li key={colIndex}>
                 <button
                   onClick={() => {
-                    symbolHandler(rowIndex, colIndex);
+                    onSelectSquare(rowIndex, colIndex);
                   }}
+                  disabled={playerSymbol !== null}
                 >
                   {playerSymbol}
                 </button>
