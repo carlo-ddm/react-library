680dca4d07bcc621c3ddef7bcbdab42b4f76fad7
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2023-12-01 17:24:54 +0100

    documentazione

--- 00-Demos/09-starting-project/src/components/QuestionTimer.jsx
+++ 00-Demos/09-starting-project/src/components/QuestionTimer.jsx
@@ -3,16 +3,18 @@ import { useEffect, useState } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
   useEffect(() => {
-    console.log('timeout');
-    setTimeout(onTimeout, timeout);
+    console.log("timeout");
+    const timer = setTimeout(onTimeout, timeout);
+    return () => clearTimeout(timer);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('interval');
-    setInterval(() => {
-      setRemainingTime((prevRemainingTime) =>  prevRemainingTime - 100 );
+    console.log("interval");
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+    return () => clearInterval(interval);
   }, []);
-  
+
   return <progress id="question-time" max={timeout} value={remainingTime} />;
 }
